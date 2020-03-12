964b8af5e85cc52b067862ec6650b256c90dff1a
Fabio <fabio_jesus@live.com>
2020-03-12 23:33:31 +0000

    Added Printer

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Common;
 using Ficha10;
 
 namespace Consola
@@ -7,32 +8,15 @@ namespace Consola
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("************************\n************************\n**                    **\n**");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("     Módulo 4       ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("**\n**                    **");
-                Console.Write("************************\n************************");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\n\n");
-                var num = Common.InputRequest.RequestInteger("Seleccione a ficha:");
-                if (num < 10 && num > 15) continue;
-                switch (num)
-                {
-                    case 10: Ficha10.Menu.Run();break;
-                    case 11: break;
-                    case 12: break;
-                    case 13: break;
-                    case 14: break;
-                    case 15: break;
-                    default: continue;
-                }
-            }
+            Menu.Instance.Load();
+            Console.WriteLine("eowkqek");
+            Console.WriteLine("eowkqek");
+            Console.WriteLine("eowkqek");
+            Console.WriteLine("eowkqek");
+            Console.WriteLine("eowkqek");
+            Console.WriteLine("eowkqek");
+            Menu.Instance.ClearRequest();
             
-
 
         }
     }
